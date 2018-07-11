8c1d1d89650a2f58405ffe857c0db61751272675
GenialX <admin@ihuxu.com>
2018-07-11 20:02:04 +0800

    :wrench: add src code

---
+++ mf-numpy-pandas/python1.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+import numpy as np
+
+array = np.array([[1,2,3],
+                  [2,3,4]])
+
+print(array)
+print(array.ndim)
+print(array.shape)
+print(array.size)
---
+++ scipy-pl/1.py
@@ -0,0 +1,5 @@
+#!/usr/local/bin/python3
+from scipy import constants
+
+print(constants.pi)
+print(constants.golden)
